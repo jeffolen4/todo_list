df92c94f37ddf309cd174f5fe3849378339597fb
LEARN01 <learn01@LEARN01.local>
2015-01-16 11:37:18 -0800

    multi-list working initial version

--- js/todo-list.ui.js
+++ js/todo-list.ui.js
@@ -9,21 +9,67 @@ $(document).ready(function() {
   var desc = $("#desc");
   var status = $("#status");
 
+  var currentList = "default-list";
+
+
+  var currentItem;
+
+  $(".alllists").hide();
+  $("#" + currentList).show();
+
+  // dropdown box to select the to-do list to work with
+  var selectType = $("#selectlist");
+
+  // button to add new list type
+  var addType = $("#addtype");
+
+  // button to close/delete a list item
+  var closeItem = $("#closeitem");
+
+  // input form for adding new items
   var inputForm = $("form");
-  //console.log("running document.ready.")
 
+  function clearDetails () {
+    $(".taskname").text("");
+    $(".taskdesc").text("");
+    $(".taskstatus").text("");
+  }
+
+  // process the input form
   inputForm.submit( function (e) {
-    //console.log("captured submit event.")
     e.preventDefault();
-    var newTask = new listItem( listType, task.val(), desc.val(), status.val() );
+    var newTask = new listItem( currentList, task.val(), desc.val(), status.val() );
+    $("#task").val("");
+    $("#desc").val("");
+    $("#status").val("");
 
-    $("#task-item-line").append("<p class=\"taskitem\" >" + newTask.task + "</p>");
-    $("p.taskitem").last().click( function () {
+    $("#" + currentList).append("<p class=\"taskitem\" >" + newTask.task + "</p>");
+    $(".taskitem").last().click( function () {
+       currentItem = $(this);
        $(".taskname").text(newTask.task);
        $(".taskdesc").text(newTask.taskDesc);
        $(".taskstatus").text(newTask.taskStatus);
      });
 
   });
+
+
+  // process changing the list type
+  selectType.change( function () {
+    $(".alllists").hide();
+    currentList = selectType.val();
+    $("#" + currentList).show();
+    clearDetails();
+  });
+
+  // process close/delete item
+  closeItem.click( function () {
+    clearDetails();
+    if ( !(currentItem) ) {
+      return false;
+    } else {
+      currentItem.remove();
+    }
+  })
 
 });
